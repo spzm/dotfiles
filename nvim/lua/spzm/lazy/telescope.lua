dfb8e1869daa922950ff04acf4124eca82b04e37
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2024-04-07 23:25:45 +0200

    feat: add fzf for telescope

--- nvim/lua/spzm/lazy/telescope.lua
+++ nvim/lua/spzm/lazy/telescope.lua
@@ -4,11 +4,17 @@ return {
   tag = "0.1.5",
 
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    }
   },
 
   config = function()
     require('telescope').setup({})
+    pcall(require('telescope').load_extension, 'fzf')
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
