51f41b136000e1ba2d237f2e20f09d317fbe53a5
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2024-05-22 22:48:48 +0200

    feat: nvim update, config tune up

--- nvim/lua/spzm/lazy/telescope.lua
+++ nvim/lua/spzm/lazy/telescope.lua
@@ -1,19 +1,61 @@
 return {
-  "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-  tag = "0.1.5",
+	tag = "0.1.6",
 
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "BurntSushi/ripgrep",
-    {
-      'nvim-telescope/telescope-fzf-native.nvim',
-      build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-    }
-  },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"BurntSushi/ripgrep",
+		{
+			'nvim-telescope/telescope-fzf-native.nvim',
+			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+		}
+	},
 
-  config = function()
-    require('telescope').setup({})
+	config = function()
+		require('telescope').setup({
+			defaults = {
+				-- configure to use ripgrep
+				vimgrep_arguments = {
+					"rg",
+					"--follow",        -- Follow symbolic links
+					"--hidden",        -- Search for hidden files
+					"--no-heading",    -- Don't group matches by each file
+					"--with-filename", -- Print the file path with the matched lines
+					"--line-number",   -- Show line numbers
+					"--column",        -- Show column numbers
+					"--smart-case",    -- Smart case search
+
+					-- Exclude some patterns from search
+					"--glob=!**/.git/*",
+					"--glob=!**/.idea/*",
+					"--glob=!**/.vscode/*",
+					"--glob=!**/build/*",
+					"--glob=!**/dist/*",
+					"--glob=!**/yarn.lock",
+					"--glob=!**/package-lock.json",
+				},
+			},
+			pickers = {
+				find_files = {
+					hidden = true,
+					-- needed to exclude some files & dirs from general search
+					-- when not included or specified in .gitignore
+					find_command = {
+						"rg",
+						"--files",
+						"--hidden",
+						"--glob=!**/.git/*",
+						"--glob=!**/.idea/*",
+						"--glob=!**/.vscode/*",
+						"--glob=!**/build/*",
+						"--glob=!**/dist/*",
+						"--glob=!**/yarn.lock",
+						"--glob=!**/package-lock.json",
+					},
+				},
+			},
+		})
     pcall(require('telescope').load_extension, 'fzf')
 
     local builtin = require('telescope.builtin')
--- nvim/lua/spzm/remap.lua
+++ nvim/lua/spzm/remap.lua
@@ -11,20 +11,17 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
+  require("vim-with-me").StartVimWithMe()
 end)
 vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
+  require("vim-with-me").StopVimWithMe()
 end)
 
--- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
-
--- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
@@ -41,15 +38,14 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set(
-    "n",
-    "<leader>ee",
-    "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
+  "n",
+  "<leader>ee",
+  "oif err != nil {<CR>}<Esc>Oreturn err<Esc>"
 )
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
-
