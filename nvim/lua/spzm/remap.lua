b73d54dff76a6cd0824d5671f07c2ce1b2dc9503
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2024-08-11 22:04:09 +0200

    feat: update packages, update windows move bindings

--- nvim/lua/spzm/remap.lua
+++ nvim/lua/spzm/remap.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Vim panes navigation
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
