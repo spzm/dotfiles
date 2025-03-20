935e5153bd8639fd37ae00942ece3b8c1d66d167
Uladzimir Dziomin <uladzimir.dziomin@gmail.com>
2025-03-20 22:09:12 +0100

    feat: update telescope key bindings

--- nvim/lua/spzm/lazy/telescope.lua
+++ nvim/lua/spzm/lazy/telescope.lua
@@ -70,12 +70,14 @@ return {
 
     vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope buffers' })
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-
     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
     vim.keymap.set('n', '<leader>ps', function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end)
+    vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
+
     vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
     vim.keymap.set('n', '<leader>pws', function()
       local word = vim.fn.expand("<cword>")
       builtin.grep_string({ search = word })
